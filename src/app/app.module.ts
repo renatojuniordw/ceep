172e8b424239cc4e610987b59c904343121a2635
Carolina Melo <cmpm@cin.ufpe.br>
2018-05-31 00:54:59 -0300

    Adição de Cartão, parcialmente implementada (cartão não está adicionando)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
-import{ FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { OpcoesDaPaginaComponent } from './components/opcoes-da-pagina/opcoes-da-pagina.component';
 import { FormNovoCartaoComponent } from './components/form-novo-cartao/form-novo-cartao.component';
--- src/app/components/form-novo-cartao/form-novo-cartao.component.ts
+++ src/app/components/form-novo-cartao/form-novo-cartao.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CartaoComponent } from '../cartao/cartao.component';
 
 @Component({
   selector: 'app-form-novo-cartao',
-  templateUrl: './form-novo-cartao.component.html',
-  styleUrls: ['./form-novo-cartao.component.css']
+  templateUrl: './form-novo-cartao.component.html'
 })
 export class FormNovoCartaoComponent implements OnInit {
+  httpClient: HttpClient;
+  cartao: CartaoComponent = {
+    id: '',
+    conteudo: '',
+    corCartao: ''
+  };
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    this.httpClient = http;
+  }
+
+  criaCartao() {
+    console.log(this.cartao);
+    // this.httpClient
+    //   .post(`https://ceep.herokuapp.com/cartoes/salvar`, this.cartao)
+    //   .subscribe(() => {
+    //     console.log('cadastrou!');
+    //     console.log(this.cartao);
+    //   });
+  }
 
   ngOnInit() {
   }
-
 }
